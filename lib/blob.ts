62b4da0b63ecafde4a8dc9283aeb908a494aa7dc
Md Towfiq Alahi <towfiq.ytpro@gmail.com>
2026-03-04 14:23:47 +0600

    Add image upload, list, and delete functions

---
+++ lib/blob.ts
@@ -0,0 +1,16 @@
+import { put, list, del } from "@vercel/blob"
+
+export async function uploadImage(name: string, file: File) {
+  return await put(name, file, {
+    access: "public"
+  })
+}
+
+export async function listImages() {
+  const { blobs } = await list()
+  return blobs
+}
+
+export async function deleteImage(url: string) {
+  await del(url)
+}
